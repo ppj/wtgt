ec4a4c4a4cd86ef97a36ee50e2d558b8f61357cc
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-04-16 12:29:36 +1000

    added some sample places & destinations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+place1 = Place.create(name: "Pune", country: "India")
+place2 = Place.create(name: "Melbourne", country: "Australia")
+destination1 = Destination.create(category: "International", place: place1)
+destination2 = Destination.create(category: "Local", place: place2)
+
