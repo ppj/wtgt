65d4f710e1de945667dfc597445d01a004816fc4
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-04-28 09:19:29 +1000

    place name must be unique for a country

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ActiveRecord::Base
   validates_presence_of :name, :country
+  validates_uniqueness_of :name, scope: :country
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,4 +1,5 @@
 describe Place do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:country) }
+  it { should validate_uniqueness_of(:name).scoped_to(:country) }
 end
