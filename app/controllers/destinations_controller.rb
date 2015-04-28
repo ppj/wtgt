10480131d118d601610fb80f583d11412e1838bd
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-04-28 12:50:14 +1000

    user can now add a destination to their wishlist

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -8,4 +8,29 @@ class DestinationsController < ApplicationController
   def new
     @destination = Destination.new
   end
+
+  def create
+    @place = Place.find_by(name: params[:destination][:place][:name], country: params[:destination][:place][:country])
+    @place ||= Place.new(place_params)
+    @place.save if @place.new_record?
+    @destination = current_user.destinations.new(destination_params)
+    @destination.place = @place
+    if @destination.save
+      flash[:success] = "Added destination to your wishlist!"
+      redirect_to destinations_path
+    else
+      flash.now[:danger] = "Place inputs seem to have error(s)"
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params[:destination].require(:place).permit(:name, :country)
+  end
+
+  def destination_params
+    params.require(:destination).permit(:category, :visited)
+  end
 end
--- spec/controllers/destinations_controller_spec.rb
+++ spec/controllers/destinations_controller_spec.rb
@@ -31,9 +31,79 @@ describe DestinationsController do
       let(:bob) { Fabricate :user }
       before { set_current_user bob }
 
-      it "sets @destination to a new Destination record" do
+      it "sets @destination to a new Destination" do
         get :new
         expect(assigns(:destination)).to be_a_new(Destination)
+      end
+    end
+  end
+
+  describe "POST create" do
+    it_behaves_like "a security guard" do
+      let(:action) { post :create }
+    end
+
+    context "with a signed in user" do
+      context "with valid inputs" do
+        let(:bob) { Fabricate :user }
+        before { set_current_user bob }
+
+        it "creates a new place if it doesn't exist already" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(Place.count).to eq(1)
+        end
+
+        it "uses an existing place if found" do
+          place = Place.create(name: "Pune", country: "India")
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(Place.count).to eq(1)
+          expect(Destination.first.place).to eq(place)
+        end
+
+        it "creates a new destination for the user" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(bob.destinations.count).to eq(1)
+        end
+
+        it "displays a message about successful creation of the destination" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(flash[:success]).to be_present
+        end
+
+        it "redirects to the user's destinations page" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(response).to redirect_to destinations_path
+        end
+      end
+
+      context "with invalid inputs" do
+        let(:place) { Place.create(name: "Pune", country: "India") }
+        let(:bob)   { Fabricate :user }
+
+        before do
+          set_current_user bob
+          bob.destinations.create(place: place)
+        end
+
+        it "does not create a destination when a new place cannot be created" do
+          post :create, destination: {category: "test", place: {name: nil, country: "India"}, visited: false}
+          expect(bob.destinations.count).to eq(1)
+        end
+
+        it "does not create a destination when the place is already in the user's wishlist" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(bob.destinations.count).to eq(1)
+        end
+
+        it "displays an error message" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(flash[:danger]).to be_present
+        end
+
+        it "re-renders the new destinations form" do
+          post :create, destination: {category: "test", place: {name: "Pune", country: "India"}, visited: false}
+          expect(response).to render_template :new
+        end
       end
     end
   end
