aff4cbe91f781824914773ba1984d1fc607d473c
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-04-28 11:16:17 +1000

    user is necessary & a place can be added only once by a user

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,6 +2,8 @@ class Destination < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
   validates_presence_of :place_id
+  validates_presence_of :user_id
+  validates_uniqueness_of :place_id, scope: :user_id
 
   delegate :name, to: :place
 end
--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,13 +1,13 @@
 describe Destination do
   it { should belong_to(:user) }
-
   it { should belong_to(:place) }
-
   it { should validate_presence_of(:place_id) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_uniqueness_of(:place_id).scoped_to(:user_id) }
 
   describe "#name" do
     it "returns the name of the place" do
-      destination = Fabricate(:destination)
+      destination = Fabricate(:destination, user: Fabricate(:user))
       expect(destination.name).to eq(destination.place.name)
     end
   end
